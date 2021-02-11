2570a486640050d095b4fce69acc90e33be110d2
Anna <hasselstromanna@gmail.com>
2021-02-11 18:04:57 +0100

    Add style object in Favortes too.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer>
-      <p tabindex="0" className="heading">CONTACT</p>
-      <p tabindex="0" className="contact-info">Anna Hasselström</p>
-      <p tabindex="0" className="contact-info"> + 46 70 544 10 14</p>
-      <p tabindex="0" className="contact-info">hasselstromanna@me.com</p>
+      <p tabIndex="0" className="heading">CONTACT</p>
+      <p tabIndex="0" className="contact-info">Anna Hasselström</p>
+      <p tabIndex="0" className="contact-info"> + 46 70 544 10 14</p>
+      <p tabIndex="0" className="contact-info">hasselstromanna@me.com</p>
     </footer>
   )
 };
